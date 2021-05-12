9a00fd5ff5b6292b1f48cb84506fbedf67d78116
Henning Buhl <henning.buhl@yahoo.de>
2021-05-12 12:36:33 +0200

    Added hybrid net experiments

--- HybridNet/HybridNet.py
+++ HybridNet/HybridNet.py
@@ -7,6 +7,7 @@ from torchvision import datasets, transforms
 import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
+from torchviz import make_dot
 
 import qiskit
 from qiskit import transpile, assemble
@@ -56,3 +57,71 @@ class QuantumCircuit:
         return np.array([expectation])
 
 
+class HybridFunction(Function):
+    """ Hybrid quantum - classical function definition """
+
+    @staticmethod
+    def forward(ctx, input, quantum_circuit, shift):
+        """ Forward pass computation """
+        ctx.shift = shift
+        ctx.quantum_circuit = quantum_circuit
+
+        expectation_z = ctx.quantum_circuit.run(input[0].tolist())
+        result = torch.tensor([expectation_z])
+        ctx.save_for_backward(input, result)
+
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """ Backward pass computation """
+        input, expectation_z = ctx.saved_tensors
+        input_list = np.array(input.tolist())
+
+        shift_right = input_list + np.ones(input_list.shape) * ctx.shift
+        shift_left = input_list - np.ones(input_list.shape) * ctx.shift
+
+        gradients = []
+        for i in range(len(input_list)):
+            expectation_right = ctx.quantum_circuit.run(shift_right[i])
+            expectation_left = ctx.quantum_circuit.run(shift_left[i])
+
+            gradient = torch.tensor([expectation_right]) - torch.tensor([expectation_left])
+            gradients.append(gradient)
+        gradients = np.array([gradients]).T
+        return torch.tensor([gradients]).float() * grad_output.float(), None, None
+
+
+class Hybrid(nn.Module):
+    """ Hybrid quantum - classical layer definition """
+
+    def __init__(self, backend, shots, shift):
+        super(Hybrid, self).__init__()
+        self.quantum_circuit = QuantumCircuit(1, backend, shots)
+        self.shift = shift
+
+    def forward(self, input):
+        return HybridFunction.apply(input, self.quantum_circuit, self.shift)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.dropout = nn.Dropout2d()
+        self.fc1 = nn.Linear(256, 64)
+        self.fc2 = nn.Linear(64, 1)
+        self.hybrid = Hybrid(qiskit.Aer.get_backend('qasm_simulator'), 100, np.pi / 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = self.dropout(x)
+        x = x.view(1, -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.hybrid(x)
+        return torch.cat((x, 1 - x), -1)
--- HybridNet/test/TestHybridNet.py
+++ HybridNet/test/TestHybridNet.py
@@ -28,7 +28,7 @@ class TestHybridNet(TestCase):
         args.deutsch_jozsa_oracle = 'constant'  # balanced or constant
         self.args = args
 
-    def test_net(self):
+    def test_dummy_run(self):
         simulator = qiskit.Aer.get_backend('qasm_simulator')
         circuit = QuantumCircuit(1, simulator, 100)
         print('Expected value for rotation pi {}'.format(circuit.run([np.pi])[0]))
